706f7b692cbf458381e97b95a28c293266d53c14
do2523 <do262431@gmail.com>
2025-07-14 20:51:53 -0400

    Added Admin Dashboard

--- lib/screens/admindashboard.dart
+++ lib/screens/admindashboard.dart
@@ -1,22 +1,56 @@
 import 'package:flutter/material.dart';
-import 'package:shpeucfmobile/widgets/custom_button.dart';
-import 'package:flutter_svg/flutter_svg.dart';
-import 'package:shpeucfmobile/screens/login.dart';
-import 'package:shpeucfmobile/screens/SignUp.dart';
+import 'package:shpeucfmobile/landing.dart';
+import 'package:shpeucfmobile/screens/CodeScanner.dart';
+import 'package:shpeucfmobile/widgets/custom_bottom_nav_bar.dart';
 
-class AdminDashboard extends StatelessWidget {
-  const AdminDashboard({super.key});
+class Admindashboard extends StatefulWidget {
+  const Admindashboard({super.key});
+
+  @override
+  _AdmindashboardState createState() => _AdmindashboardState();
+}
+
+class _AdmindashboardState extends State<Admindashboard> {
+  int _selectedIndex = 0;
+
+  final List<Widget> _pages = [
+    Center(child: Landing()),
+    Center(
+      child: Text('Instagram Page', style: TextStyle(color: Colors.white)),
+    ),
+    Center(child: Text('Calendar Page', style: TextStyle(color: Colors.white))),
+    CodeScanner(),
+    Center(child: Text('Members Page', style: TextStyle(color: Colors.white))),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      extendBody: true,
       body: Stack(
         fit: StackFit.expand,
-        // Background image
         children: [
           Image.asset('lib/images/background.png', fit: BoxFit.cover),
-      ]),
+          Column(
+            children: [
+              Expanded(child: _pages[_selectedIndex]),
+              Padding(
+                padding: const EdgeInsets.only(left: 20, right: 20, bottom: 25),
+                child: CustomBottomNavBar(
+                  currentIndex: _selectedIndex,
+                  onTap: _onItemTapped,
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
     );
-    
   }
 }
